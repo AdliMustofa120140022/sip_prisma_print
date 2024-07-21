34a0e6cfe0f97ed3e540f4e6055a39aa2184f843
AdliMustofa120140022 <aadlimustofa@gmail.com>
2024-07-21 19:35:17 +0700

    feat() - fix prod

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Spatie\LaravelIgnition\Facades\Flare;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,5 +19,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-        Flare::handles($exceptions);
+        // ::handles($exceptions);
     })->create();
