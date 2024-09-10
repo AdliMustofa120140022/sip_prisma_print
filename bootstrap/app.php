aa33cb1aa0ceb9dcfa31880a81adff87c7aa2449
AdliMustofa120140022 <aadlimustofa@gmail.com>
2024-09-10 16:22:04 +0700

    feat() - shoping cart

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\admin::class,
             'superadmin' => \App\Http\Middleware\superAdmin::class,
-            'user' => \App\Http\Middleware\user::class,
+            'user' => \App\Http\Middleware\User::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
