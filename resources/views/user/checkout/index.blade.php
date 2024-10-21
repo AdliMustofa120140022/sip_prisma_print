2e40decdaa5d99a51955a7177e08c78bbe6c65ce
AdliMustofa120140022 <aadlimustofa@gmail.com>
2024-10-21 21:02:45 +0700

    setup() - fix

--- resources/views/user/checkout/index.blade.php
+++ resources/views/user/checkout/index.blade.php
@@ -67,7 +67,10 @@
                                     </p>
                                 </div>
 
-                                <div class="px-0 md:px-3" x-data="productCounter(@json($produk_transaksi->id), @json($produk_transaksi->jumlah), @json($produk_transaksi->produck->data_produck->harga_satuan), '{{ route('user.checkout.update-quantity') }}', {{ $produk_transaksi->produck->data_produck->stok }})" x-init='calculateTotalPrice'>
+                                <div class="px-0 md:px-3" x-data=`productCounter(@json($produk_transaksi->id),
+                                    @json($produk_transaksi->jumlah),
+                                    @json($produk_transaksi->produck->data_produck->harga_satuan),"{{ route('user.checkout.update-quantity') }}",
+                                    @json($produk_transaksi->produck->data_produck->stok) )` x-init='calculateTotalPrice'>
                                     <p class="font-bold text-base">Jumlah</p>
                                     <div class="flex items-center">
                                         <button type="button"
