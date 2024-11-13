fccf43168bc5c9f9bd507e1d8730ddb4eb1988b6
Adli Mustofa <aadlimustofa@gmail.com>
2024-11-13 14:48:55 +0700

    Update meta-seo.blade.php

--- resources/views/components/utils/meta-seo.blade.php
+++ resources/views/components/utils/meta-seo.blade.php
@@ -1,6 +1,6 @@
  <!-- Meta SEO -->
  <meta name="description"
-     content="Percetakan Prima Printing adalah layanan percetakan profesional yang menawarkan berbagai solusi cetak berkualitas tinggi, termasuk undangan, kartu nama, brosur, banner, pamflet, sertifikat dan lainnya dengan harga terjangkau dan layanan cepat.">
+     content="Percetakan Prima Printing adalah layanan percetakan profesional dan percetakan murah Lampung yang menawarkan berbagai solusi cetak berkualitas tinggi, termasuk undangan, kartu nama, brosur, banner, pamflet, sertifikat dan lainnya dengan harga terjangkau dan layanan cepat.">
  <meta name="keywords"
      content="Percetakan Prima Printing, Percetakan Kotabumi, Prima Printing, Percetakan Prokimal, Fotocopy Madukoro, Cetak Undangan Pernikahan dan Khitanan, Cetak Kartu Nama, Cetak Brosur, Cetak Banner, Layanan Percetakan Profesional, Cetak Online Murah, Percetakan Cepat, Cetak Murah, Spanduk, Packaging, Percetakan Offset dan Digital, Percetakan Full Color, Cetak Kustom Online, Desain Kartu Nama, Cetak Buku dan Kalender, Cetak Poster dan Flyer, Cetak ID Card dan Sertifikat, Percetakan di Madukoro, Percetakan di Prokimal, Fotocopy Cempaka, Percetakan Berkualitas, Jasa Cetak Murah, Undangan Lampung, Percetakan Lampung, Prima Printing Layanan Terbaik">
  <meta name="author" content="Prima Printing">
@@ -9,7 +9,7 @@
  <!-- Meta Open Graph (untuk media sosial seperti Facebook) -->
  <meta property="og:title" content="Prima Printing - Layanan Percetakan Profesional">
  <meta property="og:description"
-     content="Percetakan Prima Printing adalah layanan percetakan profesional yang menawarkan berbagai solusi cetak berkualitas tinggi, termasuk undangan, kartu nama, brosur, banner, pamflet, sertifikat dan lainnya dengan harga terjangkau dan layanan cepat.">
+ content="Percetakan Prima Printing adalah layanan percetakan profesional dan percetakan murah Lampung yang menawarkan berbagai solusi cetak berkualitas tinggi, termasuk undangan, kartu nama, brosur, banner, pamflet, sertifikat dan lainnya dengan harga terjangkau dan layanan cepat.">
  <meta property="og:image" content="{{ asset('static/img/prima_logo.png') }}">
  <!-- Ganti dengan URL logo atau gambar yang representatif -->
  <meta property="og:url" content="{{ url('/') }}">
